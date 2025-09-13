fcda70101d1db09d6487bd74c5f1bc189f8071b6
Aarifansarikht <arifansarikhtz@gmail.com>
2025-09-13 17:04:05 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
-import MapComponent from "./components/Map";
+import dynamic from "next/dynamic";
 
+const MapComponent = dynamic(() => import("./components/Map"), { ssr: false });
 export default function Home() {
   return (
     <main className="p-4 min-h-screen bg-green-50 relative overflow-hidden">
